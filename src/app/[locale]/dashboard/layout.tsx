5304bc4dc3c286b4396c2fd5da4e413514548e04
Neill Bryan Rivera Livia <nrivera@acity.com.pe>
2025-05-28 19:40:13 -0500

    avance
    avance

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -13,28 +13,45 @@ export default function DashboardLayout({
   const pathname = usePathname();
   return (
     <div
-      className={`${stripLocalePage(pathname)} app h-dvh w-full flex flex-row`}
+      className={`${stripLocalePage(pathname)} app h-dvh w-full flex flex-row `}
     >
-      <aside className="min-w-[300px] h-full bg-[#0b0809]  shadow-3xl fixed top-0 ">
-        d
+      <aside
+        className=" min-w-[300px] h-full fixed bg-[#2a3042] hidden sm:hidden md:hidden lg:block xl:block"
+        style={{ boxShadow: "0 .75rem 1.5rem #12263f08" }}
+      >
+        x
       </aside>
-      <div className="app flex flex-col ml-[300px]">
-        <header className=" bg-white shadow-3xl sticky top-0 ">
-          <div className="px-[30px] flex flex-row items-center justify-between  min-h-[70px]">
-            <div>x</div>
-            <div>xx</div>
+      <main className="flex-1  flex h-full flex-col pl-0 sm:pl-0 md:pl-0 lg:pl-[300px]">
+        <header
+          style={{ boxShadow: "0 .75rem 1.5rem #12263f08" }}
+          className="  flex flex-row items-center justify-center bg-white w-full sticky top-0 left-0 w-full px-[18px]"
+        >
+          <div className="flex min-h-[70px] flex-row items-center justify-between w-full">
+            <div>Lorem.</div>
+            <div>Assumenda.</div>
           </div>
         </header>
-        <main className="flex-1 bg-[#f8f9fa] ">
-          <div className="p-[30px]">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
-            quos architecto molestias dolorem eos eum nulla qui soluta quam
-            delectus earum, et unde ratione modi ipsam voluptatum ea similique
-            harum!
-            <div className="h-dvh"></div>
+        <div className="app bg-[#f8f8fb] p-[18px]">
+          <div className="breadcrumb flex flex-col sm:flex-row md:flex-row  items-start sm:items-center md:items-center justify-start sm:justify-between md:justify-between min-h-[36px] mb-[18px]">
+            <h1 className="font-bold ">DASHBOARD</h1>
+            <div className="breadcrumbs text-sm !p-0">
+              <ul className="text-gray-400">
+                <li>
+                  <a>Home</a>
+                </li>
+                <li>
+                  <a>Documents</a>
+                </li>
+                <li>Add Document</li>
+              </ul>
+            </div>
           </div>
-        </main>
-      </div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet eum
+          dolores ullam perferendis id dicta! Ad qui, ipsum neque quas, vitae
+          nostrum ea non possimus, modi in consequatur tempore explicabo. div.
+          <div className="h-[1000px]"></div>
+        </div>
+      </main>
     </div>
   );
 }
